29ca57aae41c5773ebb8995ec6b07b4088c869cd
Takuho3650 <s1f102101536@iniad.org>
2023-10-06 16:16:09 +0900

    モデルとviewsの実装

--- stamp/views.py
+++ stamp/views.py
@@ -1,3 +1,4 @@
+from .models import Stamp
 from django.shortcuts import render
 from django.views.generic import TemplateView, RedirectView
 
@@ -5,6 +6,35 @@ from django.views.generic import TemplateView, RedirectView
 
 class stamp(TemplateView):
     template_name = "stamp/stamp.html"
+    def get_context_data(self, **kwargs):
+        try:
+            user_info = Stamp.objects.get(user=self.request.user)
+        except Stamp.DoesNotExist:
+            Stamp.objects.create(user=self.request.user, stamps=[False,False,False,False,False,False])
+            user_info = Stamp.objects.get(user=self.request.user)
+            print("ユーザー情報を新規作成しました。")
+
+        context = super().get_context_data(**kwargs)
+        context["user"] = user_info.user
+        context["stamps"] = user_info.stamps
+
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        try:
+            user_info = Stamp.objects.get(user=self.request.user)
+        except Stamp.DoesNotExist:
+            Stamp.objects.create(user=self.request.user, stamps=[False,False,False,False,False,False])
+            user_info = Stamp.objects.get(user=self.request.user)
+            print("ユーザー情報を新規作成しました。")
+        
+        update_stamp = user_info.stamps
+        update_stamp[request.POST["stamp_num"]] = True
+
+        user_info.stamps = update_stamp
+        user_info.save()
+
+        return rd_index(request)
 
 class redirect_stamp(RedirectView):
     url = "http://127.0.0.1:8000/stamp/"
