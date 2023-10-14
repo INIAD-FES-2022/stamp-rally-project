168399072fe235cb1d7a5ca96905394e3d8ce5cd
Takuho3650 <s1f102101536@iniad.org>
2023-10-15 08:23:55 +0900

    スタンプラリーの仕様変更に伴うアップデート
    QRコード読み込みでスタンプ付与を行えるように

--- stamp/views.py
+++ stamp/views.py
@@ -3,39 +3,61 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, RedirectView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# ここにQRが持つクエリパラメータを陳列
+# https://stampのトップページURL/?sponser=クエリパラメータ
+query_lst = ["dummy0", "dummy1", "dummy2", "dummy3", "dummy4", "dummy5"]
 
 class stamp(LoginRequiredMixin, TemplateView):
     template_name = "stamp/stamp.html"
     def get_context_data(self, **kwargs):
+
+        # ユーザー情報取得
         try:
             user_info = Stamp.objects.get(user=self.request.user)
+
+        # 初期設定
         except Stamp.DoesNotExist:
             Stamp.objects.create(user=self.request.user, stamps=[False,False,False,False,False,False])
             user_info = Stamp.objects.get(user=self.request.user)
             print("ユーザー情報を新規作成しました。")
 
+        # htmlに渡すテンプレートの値
+        # {{ user }} や {{ stamps }} で取得可能
+
+        # {{ user }} は各ユーザーが持つ一意の文字列
+        # {{ stamps }} はbool値のリスト
         context = super().get_context_data(**kwargs)
         context["user"] = user_info.user
         context["stamps"] = user_info.stamps
 
+        # スタンプ付与
+        if "sponser" in self.request.GET:
+            query = self.request.GET.get("sponser")
+            if query in query_lst:
+                update_stamps = user_info.stamps
+                update_stamps[query_lst.index(query)] = True
+                
+                user_info.stamps = update_stamps
+                user_info.save()
+
+                context["stamps"] = update_stamps
+
+            # デバッグ用
+            elif query == "reset":
+                update_stamps = user_info.stamps
+                update_stamps = [False,False,False,False,False,False]
+                
+                user_info.stamps = update_stamps
+                user_info.save()
+
+                context["stamps"] = update_stamps
+
+            # デバッグ用                
+            elif query == "print":
+                print(context["user"])
+                print(context["stamps"])
+
         return context
-    
-    def post(self, request, *args, **kwargs):
-        try:
-            user_info = Stamp.objects.get(user=self.request.user)
-        except Stamp.DoesNotExist:
-            Stamp.objects.create(user=self.request.user, stamps=[False,False,False,False,False,False])
-            user_info = Stamp.objects.get(user=self.request.user)
-            print("ユーザー情報を新規作成しました。")
-        
-        update_stamp = user_info.stamps
-        update_stamp[request.POST["stamp_num"]] = True
-
-        user_info.stamps = update_stamp
-        user_info.save()
-
-        return rd_index(request)
 
 class redirect_stamp(RedirectView):
     url = "http://127.0.0.1:8000/stamp/"
