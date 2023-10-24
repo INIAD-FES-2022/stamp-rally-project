a4ce16fdfe8b1211a5d7056e6c54a8006fffae51
Takuho3650 <s1f102101536@iniad.org>
2023-10-24 19:24:33 +0900

    修正

--- config/settings.py
+++ config/settings.py
@@ -148,7 +148,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-AWS_LOCATION = 'static'
+AWS_LOCATION = 'stamp/static'
 AWS_AWS_QUERYSTRING_AUTH = False
 AWS_HEADERS = {
     'Access-Control-Allow-Origin': '*',
