eb36997e3f2b279ccf85886052e21e7380cf1a67
Takuho3650 <s1f102101536@iniad.org>
2023-10-24 13:14:18 +0900

    修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+python manage.py makemigrations stamp --noinput
+python manage.py migrate --noinput
 python manage.py collectstatic --no-input --clear
 
 exec "$@"
