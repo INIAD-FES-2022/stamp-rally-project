c3d9090073d191e1d5a2649bc270fa5f536405a8
Takuho3650 <s1f102101536@iniad.org>
2023-11-02 07:15:29 +0900

    デバッグ用改変

--- stamp/urls.py
+++ stamp/urls.py
@@ -2,18 +2,6 @@ from . import views
 from django.urls import path
 from django.urls.conf import include
 from django.contrib.auth.views import LoginView, LogoutView
-from pathlib import Path
-import os, environ
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Get env
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, ".env"))
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('DJANGO_DEBUG') == 'True' else False
 
 urlpatterns = [
     path('', views.redirect_stamp.as_view(), name='redirect'),
@@ -23,6 +11,5 @@ urlpatterns = [
     path("stamp/", views.stamp.as_view(), name="stamp"),
     path("stamp/get/<str:sponser>/", views.stamp_get.as_view(), name="stamp_get"),
     path("stamp/map/", views.stamp_map.as_view(), name="stamp_map"),
+    path('test/', views.my_test_500_view, name='test'),
 ]
-if DEBUG:
-    urlpatterns[-1] = path('test/', views.my_test_500_view, name='test')
