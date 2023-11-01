94298c7fe77ecff3e8a612ba4699066c21dc0007
Takuho3650 <s1f102101536@iniad.org>
2023-11-02 06:53:54 +0900

    urlsの変更忘れ

--- stamp/urls.py
+++ stamp/urls.py
@@ -2,6 +2,18 @@ from . import views
 from django.urls import path
 from django.urls.conf import include
 from django.contrib.auth.views import LoginView, LogoutView
+from pathlib import Path
+import os, environ
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Get env
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True if os.environ.get('DJANGO_DEBUG') == 'True' else False
 
 urlpatterns = [
     path('', views.redirect_stamp.as_view(), name='redirect'),
@@ -10,9 +22,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path("stamp/", views.stamp.as_view(), name="stamp"),
     path("stamp/get/<str:sponser>/", views.stamp_get.as_view(), name="stamp_get"),
-    #path("stamp/prize/", views.stamp_prize.as_view(), name="stamp_prize"),
     path("stamp/map/", views.stamp_map.as_view(), name="stamp_map"),
-    
-    # 500エラー確認用
-    #path('test/', views.my_test_500_view, name='test')
 ]
+if DEBUG:
+    urlpatterns[-1] = path('test/', views.my_test_500_view, name='test')
